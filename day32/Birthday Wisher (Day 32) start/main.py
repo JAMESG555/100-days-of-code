e305c2abf753fba741672295f8f7a85735cae7de
root <root@DESKTOP-RP2ELTM.localdomain>
2024-06-08 14:16:05 +1000

    Day 33

--- day32/Birthday Wisher (Day 32) start/main.py
+++ day32/Birthday Wisher (Day 32) start/main.py
@@ -1,22 +1,26 @@
-# import smtplib
-# from config import my_email, password
-#
-#
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(from_addr=my_email, to_addrs="james_G5@hotmail.com", msg="Subject:Hello\n\nTest123")
-#     # connection.close()
-
 import datetime as dt
-
+import random
+import smtplib
+from config import my_email, password
 now = dt.datetime.now()
-year = now.year
 
-print(type(year))
-print(type(now))
+def send_email():
+    if get_day():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+             connection.starttls()
+             connection.login(user=my_email, password=password)
+             connection.sendmail(from_addr=my_email, to_addrs="james_G5@hotmail.com", msg=f"Subject:Monday Motivational Quote\n\n{get_quote()}")
 
-if year == 2024:
-    print(True)
-else:
-    print(False)
+
+def get_day():
+    if now.weekday() == 0:
+        return True
+    else:
+        return False
+
+def get_quote():
+    with open("quotes.txt", "r") as data:
+        quote_list = data.readlines()
+    return quote_list[random.randint(0, len(quote_list)-1)]
+
+send_email()
--- day32/main.py
+++ day32/main.py
@@ -1,22 +1,36 @@
-# import smtplib
-# from config import my_email, password
-#
-#
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(from_addr=my_email, to_addrs="james_G5@hotmail.com", msg="Subject:Hello\n\nTest123")
-#     # connection.close()
-
+import pandas
 import datetime as dt
+import random
+import smtplib
+from config import my_email, password
+##################### Hard Starting Project ######################
+today = dt.datetime.now()
+today_day = today.day
+today_month = today.month
+pd_bd = pandas.read_csv("birthdays.csv")
+birthdays_dict = pd_bd.to_dict()
 
-now = dt.datetime.now()
-year = now.year
+def find_name_by_month_day(month, day):
+    for i in range(len(birthdays_dict['month'])):
+        if birthdays_dict['month'][i] == month and birthdays_dict['day'][i] == day:
+            return i
+    return None
 
-print(type(year))
-print(type(now))
+def create_letter(name):
+    with open(f"letter_templates/letter_{random.randint(1, 3)}.txt") as data:
+        contents = data.read()
+        contents = contents.replace("[NAME]", name)
+    return contents
 
-if year == 2024:
-    print(True)
-else:
-    print(False)
+def send_email(i):
+        letter = create_letter(birthdays_dict['name'][i])
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+             connection.starttls()
+             connection.login(user=my_email, password=password)
+             connection.sendmail(from_addr=my_email, to_addrs=birthdays_dict['email'][i], msg=f"Subject:Happy Birthday!\n\n{letter}")
+
+if find_name_by_month_day(today_month, today_day):
+    send_email(find_name_by_month_day(today_month, today_day))
+
+
+
